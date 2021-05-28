462d6ed8c6715d7e5d3b72ee6218c16e963a238f
Jelmer Wiersma <jelmer.wiersma@rocva.nl>
2021-05-28 11:47:52 +0200

    mysql 1

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,2 @@
-
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) VALUES ('6969', 'Chanel', 'Burki', '7D', 'Dingelen 17', '3443TT', '3004-13-32', 'Almere');
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) VALUES ('4200', 'Donyel', 'Malen', '5D', 'Stormstraat 17', '1818XX', '2004-03-23', 'Arnhem');
