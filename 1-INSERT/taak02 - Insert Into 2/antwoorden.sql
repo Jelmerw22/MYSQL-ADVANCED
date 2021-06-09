950947ea7ea853b8b37828ed643b61663c2b2a22
Jelmer Wiersma <jelmer.wiersma@rocva.nl>
2021-06-09 11:30:32 +0200

    sql advanced

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,3 @@
-
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats) VALUES ('Wout', 'Weghorst', '1992-04-23', 'Wolfsburg');
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats) VALUES ('Luuk', 'de Jong', '1990-07-13', 'Sevilla');
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats) VALUES ('Frenkie', 'de Jong', '1998-02-01', 'Barcelona');
